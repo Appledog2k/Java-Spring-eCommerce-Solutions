76a87e1ad1f205a21bdd4caeadc0722afb8c76be
appledog2k <appledog2k@gmail.com>
2024-12-16 12:04:23 +0700

    first commit

---
+++ spring-controller/src/main/java/appledog/research/controller/resource/HiController.java
@@ -0,0 +1,14 @@
+package appledog.research.controller.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HiController {
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hello, Controller!";
+    }
+}
